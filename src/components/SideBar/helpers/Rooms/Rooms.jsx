79b180b65e0008436d2de7afe6eb1512401e591b
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-03 20:44:05 +0100

    removed a h1.

--- src/components/SideBar/helpers/Rooms/Rooms.jsx
+++ src/components/SideBar/helpers/Rooms/Rooms.jsx
@@ -5,7 +5,6 @@ import './Rooms.sass';
 
 const Rooms = () => (
   <div className="rooms">
-    <h1 className="rooms__add-new">add a new chat</h1>
     <Room title="dance" lastMessage="this is real time" />
     <Room title="dev" lastMessage="this is real time" />
     <Room title="epic" lastMessage="this is real time" />
