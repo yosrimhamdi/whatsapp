5df319f2269400f270523aebdd36423e7f7b4b8c
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-03 20:26:07 +0100

    sass everywhere.

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App/App';
-import './index.css';
+import './index.sass';
 
 ReactDOM.render(
   <React.StrictMode>
