19c04cc970c72c42eb4743accde6df6c99d27fc6
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-04 11:24:21 +0100

    added the chat header.

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 import './Chat.sass';
+import Header from './core/Header';
 
-const Chat = () => <div className="chat">this is the chat</div>;
+const Chat = () => (
+  <div className="chat">
+    <Header />
+  </div>
+);
 
 export default Chat;
---
+++ src/components/Chat/core/Header/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './styles.sass';
+import Avatar from '../../../shared/Avatar';
+import SearchIcon from '@material-ui/icons/Search';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { IconButton } from '@material-ui/core';
+
+const Header = () => (
+  <div className="header">
+    <Avatar />
+    <div>
+      <h1>dance room</h1>
+      <p className="header__last-seen">
+        last seen Fri, 04 Sep 2020 18:00:16 GMT
+      </p>
+    </div>
+    <div className="header__icons">
+      <IconButton>
+        <SearchIcon />
+      </IconButton>
+      <IconButton>
+        <AttachFileIcon />
+      </IconButton>
+      <IconButton>
+        <MoreVertIcon />
+      </IconButton>
+    </div>
+  </div>
+);
+
+export default Header;
