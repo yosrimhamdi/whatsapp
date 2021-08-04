2f34dc0c8286820936df2f27897cf47977dc1e5a
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-04 11:29:40 +0100

    fixed some styling issue.

--- src/components/Chat/core/Header/index.jsx
+++ src/components/Chat/core/Header/index.jsx
@@ -8,15 +8,15 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { IconButton } from '@material-ui/core';
 
 const Header = () => (
-  <div className="header">
+  <div className="chat-header">
     <Avatar />
     <div>
       <h1>dance room</h1>
-      <p className="header__last-seen">
+      <p className="chat-header__last-seen">
         last seen Fri, 04 Sep 2020 18:00:16 GMT
       </p>
     </div>
-    <div className="header__icons">
+    <div className="chat-header__icons">
       <IconButton>
         <SearchIcon />
       </IconButton>
