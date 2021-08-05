ce428e7c0a216a93ec618de2a67ebd6ce13b561c
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-05 18:34:50 +0100

    added new message to state.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,10 @@
-import React, { useEffect } from 'react';
-import Pusher from 'pusher-js';
+import React from 'react';
 
 import './App.sass';
 import SideBar from '../SideBar/SideBar';
 import Chat from '../Chat/Chat';
 
 const App = () => {
-  useEffect(() => {
-    Pusher.logToConsole = true;
-
-    const pusher = new Pusher('8017a44ac3256ec6cd6d', {
-      cluster: 'eu',
-    });
-
-    const channel = pusher.subscribe('messages');
-
-    channel.bind('inserted', data => {
-      console.log(data);
-    });
-  }, []);
-
   return (
     <div className="app">
       <div className="app__content">
--- src/components/Chat/core/Messages/Messages.jsx
+++ src/components/Chat/core/Messages/Messages.jsx
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Pusher from 'pusher-js';
 
 import './Messages.sass';
 import Message from '../Message/Message';
 
 export const Messages = () => {
   const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('8017a44ac3256ec6cd6d', {
+      cluster: 'eu',
+    });
+
+    const channel = pusher.subscribe('messages');
+
+    channel.bind('inserted', message => {
+      setMessages([message, ...messages]);
+    });
+  }, []);
 
   useEffect(() => {
     (async () => {
